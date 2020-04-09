1f30e63de2f406ec62fdc7f2884e6ddba9333ca5
Pascal Grossé <pascal.grosse@gmail.com>
2020-04-09 16:28:23 +0200

    Ajout d'exemples sur les formulaires GET et POST

---
+++ tp/tp17_serveur_flask/06-formulaire-méthode-GET/serveur.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request
+import datetime
+
+app = Flask(__name__, static_url_path='/static')
+
+@app.route('/')
+def index():
+  return app.send_static_file("index.html")
+
+@app.route('/style.css')
+def css():
+  return app.send_static_file("style.css")
+
+@app.route('/resultat', methods=['GET'])
+def resultat():
+  result = request.args
+  n = result['nom']
+  p = result['prenom']
+  return render_template("resultat.html", nom=n, prenom=p)
+
+app.run(debug=True)
+
